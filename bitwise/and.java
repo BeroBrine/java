e674e05928649cdb82b2fe217c4fe25ed2787946
BeroBrine <abhishekrana8818@gmail.com>
2023-02-26 14:27:20 +0530

    added bitwise 26-02-2023

---
+++ bitwise/and.java
@@ -0,0 +1,17 @@
+package bitwise;
+
+
+import java.util.*;
+
+public class and
+{
+    public static void main(String args[])
+    {
+        System.out.println(5 & 6);
+        System.out.println(6 | 5);
+        System.out.println(5 ^ 6);
+        System.out.println(~(-6));
+        System.out.println(5 << 2);
+        System.out.println(6 >> 3);
+    }
+}
